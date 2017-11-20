2822c2f15a8e4f4231ba1bebe5276a20fe2d9480
n313893254 <n313893254@126.com>
2017-11-20 17:56:14 +0800

    update

--- javascript.js
+++ javascript.js
@@ -2,25 +2,29 @@ var http = require('http')
 var querystring = require('querystring')
 
 var login = function () {
-  var postData=querystring.stringify({
-      principal:'admin',
+  var postData = querystring.stringify({
+      principal: 'admin',
       password: 'Harbor12345',
       verify: false,
   });
-  var options={
-     hostname:'119.23.129.240',
-     port:80,
-     path:'/login',
-     method:'POST',
-     headers:{
-      //'Content-Type':'application/x-www-form-urlencoded',
-      'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+
+  var options = {
+     hostname: '119.23.129.240',
+     port: 80,
+     path: '/login',
+     method: 'POST',
+     headers: {
+      'Content-Type':'application/x-www-form-urlencoded',
       'Content-Length':Buffer.byteLength(postData)
      }
   }
-  var req=http.request(options, function(res) {
+
+  var req = http.request(options, function(res) {
       console.log('Status:',res.statusCode);
       console.log('headers:',JSON.stringify(res.headers));
+      if (res.statusCode === 200) {
+        let session_id = res.headers['set-cookie'][0].split(';')[0].replace('beegosessionID=', '')
+      }
       res.setEncoding('utf-8');
       res.on('data',function(chun){
           console.log('body分隔线---------------------------------\r\n');
@@ -30,6 +34,7 @@ var login = function () {
           console.log('No more data in response.********');
       });
   });
+
   req.on('error',function(err){
       console.error(err);
   });
